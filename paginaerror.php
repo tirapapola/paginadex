d275b23aab2fff86a11f6b316a73909f5b8291e6
PC-PRESTAU\abrah <abrahamgijon@gmail.com>
2025-11-13 18:36:32 +0100

    mierdon

---
+++ paginaerror.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <h1>Has superado el número de intentos permitidos</h1>
+        <p>Por favor, espera 10 segundos para volver a intentarlo</p>
+        <a href="inicio-pagina-de-x.php">Volver al login</a>
+    </body>
+</html>
